bedd1439725f7dff4b3d15c87bfd9089335f7416
Jukka Aho <ahojukka5@gmail.com>
2026-07-01 16:03:54 +0300

    docs: add Documenter makedocs entry point
    Configure HTML site with upstream references, capability pages, and
    worked examples under docs/src/.

---
+++ docs/make.jl
@@ -0,0 +1,28 @@
+using Documenter
+using Netgen
+
+makedocs(
+    modules = [Netgen],
+    sitename = "Netgen.jl",
+    authors = "Jukka Aho",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        size_threshold = nothing,
+    ),
+    pages = [
+        "Home" => "index.md",
+        "Upstream documentation" => "external.md",
+        "Wrapped capabilities" => "capabilities.md",
+        "Not yet wrapped" => "limitations.md",
+        "Examples" => [
+            "Building geometry" => "examples/geometry.md",
+            "Meshing" => "examples/meshing.md",
+            "Refinement" => "examples/refinement.md",
+            "Mesh hierarchies & sessions" => "examples/hierarchy.md",
+            "Tags, hp-adaptivity & FEM data" => "examples/tags_hp_fem.md",
+        ],
+        "Development" => "development.md",
+    ],
+    checkdocs = :none,
+    warnonly = [:cross_references, :missing_docs],
+)
